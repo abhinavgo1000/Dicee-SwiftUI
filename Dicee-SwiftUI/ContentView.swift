c4a8496c3a1f2fc773625b95c78f76bf3c99aa1b
Abhinav Goel <abhinavgo1000@gmail.com>
2025-09-04 15:23:37 +0530

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Abhinav Goel on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
